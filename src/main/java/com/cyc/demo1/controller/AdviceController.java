882ea386119b24dfae39b76e44ea440550a6c98d
chenyuchuan <chuan_juan@163.com>
2019-04-17 09:38:16 +0800

    修改@ControllerAdvice为@RestControllerAdvice

--- src/main/java/com/cyc/demo1/controller/AdviceController.java
+++ src/main/java/com/cyc/demo1/controller/AdviceController.java
@@ -1,15 +1,14 @@
 package com.cyc.demo1.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author chenyuchuan
  */
-@ControllerAdvice
-@ResponseBody
+@RestControllerAdvice
 @Slf4j
 public class AdviceController {
 
